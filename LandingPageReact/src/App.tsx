6e940e8e1b2513367e41ecf137e2a5865ab24725
Victor dS <victordespirlet@gmail.com>
2026-01-09 11:39:35 +0100

    testing landing page react done

--- LandingPageReact/src/App.tsx
+++ LandingPageReact/src/App.tsx
@@ -2,9 +2,9 @@ import "./App.css";
 import HomePage from "./components/HomePage";
 import AboutPage from "./components/AboutPage";
 import WorkPage from "./components/WorkPage";
-// import ReviewsPage from "./components/ReviewsPage";
-// import ContactPage from "./components/ContactPage";
-import FooterPage from "./components/Footerpage"
+import ReviewsPage from "./components/ReviewsPage";
+import ContactPage from "./components/ContactPage";
+import FooterPage from "./components/FooterPage"
 
 function App() {
   return (
@@ -13,8 +13,8 @@ function App() {
         <HomePage />
         <AboutPage />
         <WorkPage />
-        {/* <ReviewsPage /> */}
-        {/* <ContactPage /> */}
+        <ReviewsPage />
+        <ContactPage />
         <FooterPage />
       </div>
     </>
--- LandingPageReact/src/components/ContactPage.tsx
+++ LandingPageReact/src/components/ContactPage.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function ContactPage() {
   return (
-    <div>
-      
+    <div className="contact-page-wrapper">
+      <h1 className="primary-heading">Have Question In Mind?</h1>
+      <h1 className="primary-heading">Let Us Help You</h1>
+      <div className="contact-form-container">
+        <input type="text" placeholder="yourmail@gmail.com" />
+        <button className="secondary-button">Submit</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ContactPage
+export default ContactPage;
